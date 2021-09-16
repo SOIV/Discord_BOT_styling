8fb29a909dad270499b9383ff43275b6f6ae5bcd
SOIV [이브] <58338357+hiddenky@users.noreply.github.com>
2021-09-17 04:12:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,37 +13,19 @@ client.on('message', (message) => {
   if(message.content === '온라인') {
     message.reply('온라인 확인!');
   }
-
-  if(message.content == '!서버정보') {
-    let img = 'https://media.discordapp.net/attachments/622810138985758743/743334765100400681/20.jpg'
-    let embed = new Discord.RichEmbed()
-      .setTitle('우리들의 생활공간 디스코드 정보')
-      .setURL()
-      .setAuthor('우리들의 생활관', img)
-      .setThumbnail()
-      .addBlankField()
-      .addField('우리들의 생활공간 디스코드', 'https://discord.gg/T55THdt')
-      .addField('우리들의 생활편집관 커미션', 'https://owolcomsn.kro.kr', true)
-      .addField('Inline field title', 'Some value here', true)
-      .addField('Inline field title', 'Some value here', true)
-      .addField('Inline field title', 'Some value here1\nSome value here2\nSome value here3\n')
-      .addBlankField()
-      .setTimestamp()
-      .setFooter('𝓱𝓲𝓭𝓭𝓮𝓷 𝓴𝔂')
     
     message.channel.send(embed)
   } else if(message.content == '!help') {
     let helpImg = 'https://images-ext-1.discordapp.net/external/RyofVqSAVAi0H9-1yK6M8NGy2grU5TWZkLadG-rwqk0/https/i.imgur.com/EZRAPxR.png';
     let commandList = [
       {name: '!ping', desc: '현재 핑 상태 (제작중)'},
-      {name: '!서버정보', desc: '생활공간 디스코드 정보'},
       {name: '!help', desc: 'commandList'},
     ];
     let commandStr = '';
     let embed = new Discord.RichEmbed()
-      .setAuthor('Help of 우리들의 생활관 BOT', helpImg)
+      .setAuthor('Help of project - UG_Styling', helpImg)
       .setColor('#186de6')
-      .setFooter(`우리들의 생활관 BOT`)
+      .setFooter(`project - UG_Styling`)
       .setTimestamp()
     
     commandList.forEach(x => {
