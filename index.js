bb9027402763481064635bb3fe63fe4876387845
SOIV [이브] <58338357+SOIV@users.noreply.github.com>
2021-11-02 22:54:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = process.env.token;
+var port = process.env.PORT || 8080;
 
 client.on('ready', () => {
   console.log('온라인!');
